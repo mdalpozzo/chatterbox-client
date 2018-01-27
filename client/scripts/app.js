c4a39a05c286bff2a40f25de46c57e10c2ad3614
mdalpozzo <marlin.dalpozzo@gmail.com>
2018-01-26 20:18:36 -0800

    all tests passing, data loading

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,7 +1,37 @@
 // YOUR CODE HERE:
 var app = {};
 
-app.init = function () {};
+app.init = function () {
+  $('#main').on('click', '.username', app.handleUsernameClick());
+  $('#send .submit').on('submit', app.handleSubmit());
+
+  // get all messages
+  
+  var allMessages;
+  var homePageMessages = 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages';
+  
+  //allMessages = app.fetch(homePageMessages);
+
+  app.fetch(homePageMessages);
+  
+  
+
+  
+
+  // var promise1 = new Promise();
+  // promise1 = app.fetch(homePageMessages);
+  // promise1.done(function() {
+  //   allMessages.results.forEach(function(message) {
+  //     app.renderMessage(message.text);
+  //   });
+  //}); 
+   
+  
+ // allMessages.results.forEach(function(message) {
+ //    app.renderMessage(message.text);
+ //  });
+
+};
 
 app.send = function(message) {
   $.ajax({
@@ -29,7 +59,17 @@ app.fetch = function (url) {
     //data: ,
     contentType: 'application/json',
     success: function (data) {
-      console.log('chatterbox: Message sent');
+      //save data 
+      allMessages = data;
+      
+      //iterate through all the data
+      allMessages.results.forEach(function(message) {
+        // display the data
+        app.renderMessage(message.text, message.username);
+        
+      });
+
+      console.log('chatterbox: Message fetched');
     },
     error: function (data) {
       console.log('chatterbox: Failed to send message', data);
@@ -41,24 +81,30 @@ app.clearMessages = function() {
   $('#chats').children().remove();
 };
 
-app.renderMessage = function(message) {
-  
-  $('#chats').append('<p>' + message.text + '</p>');
+app.renderMessage = function(message, username) {
+  $('#chats').append('<div class="chat"> <p class="username">' + username + '</p> <p>' + message + '</p></div>');
+  //$('#chats').append('<p class="chat">' + message + '</p>');
 };
 
 app.renderRoom = function(room) {
   $('#roomSelect').append('<p>' + room + '</p>');
 };
 
-//$('#main').on('click', '.username', app.handleUsernameClick());
+
 
 
 app.handleUsernameClick = function() {
-  return true;
+  
 };
 
+app.handleSubmit = function() {
+  app.send(document.getElementById('send').value);
+  document.getElementById('send').value = '';
+};
 
-var allMessages;
+app.init();
+
+
 
 // var message = {
 //   username: 'shawndrost',
@@ -101,24 +147,24 @@ var allMessages;
 // });
 
 
-$.ajax({
-  // This is the url you should use to communicate with the parse API server.
-  //url: 'https://myAppID:javascript-key=myJavaScriptKey@parse.sfm8.hackreactor.com/chatterbox/classes/messages',
+// $.ajax({
+//   // This is the url you should use to communicate with the parse API server.
+//   //url: 'https://myAppID:javascript-key=myJavaScriptKey@parse.sfm8.hackreactor.com/chatterbox/classes/messages',
 
-  url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
-  type: 'GET',
-  //data: data,
-  contentType: 'application/json',
-  success: function (data) {
-    //allMessages.push(data);
-    allMessages = data.results;
-    console.log('chatterbox: Message recieved', data);
-  },
-  error: function (data) {
-    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-    console.error('chatterbox: Failed to send message', data);
-  }
-});
+//   url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
+//   type: 'GET',
+//   //data: data,
+//   contentType: 'application/json',
+//   success: function (data) {
+//     //allMessages.push(data);
+//     allMessages = data.results;
+//     console.log('chatterbox: Message recieved', data);
+//   },
+//   error: function (data) {
+//     // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+//     console.error('chatterbox: Failed to send message', data);
+//   }
+// });
 
 // $(document).ready(function() {
 
